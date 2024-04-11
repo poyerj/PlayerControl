8e51295aafc6642740a4a729ced4da3d8a9a72e4
poyerj <poyerj@roverkids.org>
2024-04-11 13:57:33 -0400

    1.3.1

--- PlayerControl/Assets/Scripts/PlayerController.cs
+++ PlayerControl/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
