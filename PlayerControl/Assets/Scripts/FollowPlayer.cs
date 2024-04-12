4409dea9f2baec37c2e08a809d70df1897d7fc62
poyerj <poyerj@roverkids.org>
2024-04-12 13:29:14 -0400

    1.3 complete

--- PlayerControl/Assets/Scripts/FollowPlayer.cs
+++ PlayerControl/Assets/Scripts/FollowPlayer.cs
@@ -6,15 +6,16 @@ public class FollowPlayer : MonoBehaviour
 {
 
     public GameObject player;
+    private Vector3 offset = new Vector3(0, 5, -7);
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = player.transform.position;
+       
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = player.transform.position + offset;
     }
 }
